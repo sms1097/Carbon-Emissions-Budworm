ab0f6d214614114aa480ae269d412124f14cf14a
Sean Smith <sms1097@wildcats.unh.edu>
2021-01-20 00:17:00 -0500

    Initial working demo

--- src/classifier.py
+++ src/classifier.py
@@ -1,4 +1,5 @@
 import pickle 
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import RobustScaler
 
 
@@ -12,18 +13,50 @@ class Classifier():
         self.no_mgmt = pickle.load(open('model/nomgmt_classifier', 'rb'))
 
 
-    def predict(X_test, treatment):
-        if treatment == 'comm_ind':
+    def predict(self, data, management, return_y=False):
+        salvage = data[
+            (data['TimeStep'] == 40) & 
+            (data['Treatment'] == management) & 
+            (data['Salvage'] == 'Salvage')
+        ]
+
+        salvage = salvage.set_index("StandID")
+        salvage = salvage.fillna(salvage.mean())
+
+        no_salvage = data[
+            (data['TimeStep'] == 40) & 
+            (data['Treatment'] == management) & 
+            (data['Salvage'] == 'NoSalvage')
+        ]
+
+        no_salvage = no_salvage.set_index("StandID")
+        no_salvage = no_salvage.fillna(no_salvage.mean())
+
+        data = salvage.copy()
+        data["DR5"] -= no_salvage["DR5"]
+
+        data['Voucher'] = (data["DR5"] > 0)
+
+        scaler = RobustScaler()
+        X = data.drop(['Voucher', 'Treatment', 'NoDR', 'DR5', 'DR1', 'DR3', 'Salvage', 'TimeStep'], axis=1)
+        X = scaler.fit_transform(X)
+
+        y = data['Voucher']
+
+        _, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
+
+        if management == 'Comm-Ind':
             out = self.comm_ind.predict(X_test)
-        elif treatment == 'heavy':
+        elif management == 'Heavy':
             out = self.heavy.predict(X_test)
-        elif treatment == 'high_grade':
+        elif management == 'HighGrade':
             out = self.high_grade.predict(X_test)
-        elif treatment == 'light':
+        elif management == 'Light':
             out = self.light.predict(X_test)
-        elif treatment == 'moderate':
+        elif management == 'Moderate':
             out = self.moderate.predict(X_test)
-        elif treatment == 'nomgmt':
+        elif management == 'NoMgmt':
             out = self.no_mgmt.predict(X_test)
 
-        return out
+        return out, y_test
